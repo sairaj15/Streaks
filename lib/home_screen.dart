ad23a81e8bcb1639aa47b311d0074b16dd0a41ee
sairaj15 <sairajssawant@proton.me>
2026-05-04 13:43:13 +0530

    feat: v1 done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:dailylearningtracker/bloc/entry_bloc.dart';
+import 'package:dailylearningtracker/bloc/entry_event.dart';
 import 'package:dailylearningtracker/bloc/entry_state.dart';
+import 'package:dailylearningtracker/task_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dailylearningtracker/add_entry_screen.dart';
@@ -17,7 +19,6 @@ class _HomeScreenState extends State<StatefulWidget> {
     return BlocBuilder<EntryBloc, EntryState>(
       builder: (context, state) {
         if (state is StateShowEntries) {
-          // final entries = state.entries;
           return Scaffold(
             appBar: AppBar(
               title: Text(
@@ -28,20 +29,44 @@ class _HomeScreenState extends State<StatefulWidget> {
             ),
             body: Column(
               children: [
-                ListView.builder(
-                  itemCount: state.entries.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(state.entries[index].nameOfTheTask),
-                    );
-                  },
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.entries.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(state.entries[index].nameOfTheTask),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TaskScreen(
+                                nameOfTheTask:
+                                    state.entries[index].nameOfTheTask,
+                              ),
+                            ),
+                          );
+                        },
+                        trailing: ElevatedButton(
+                          onPressed: () => context.read<EntryBloc>().add(
+                            EventMarkDone(
+                              nameOfTheTask: state.entries[index].nameOfTheTask,
+                            ),
+                          ),
+                          child: Icon(Icons.check),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ],
             ),
             floatingActionButton: FloatingActionButton(
               child: Text('Add Entry'),
               onPressed: () {
-                AddEntryScreen();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddEntryScreen()),
+                );
               },
             ),
           );
---
+++ lib/task_screen.dart
@@ -0,0 +1,34 @@
+import 'package:dailylearningtracker/bloc/entry_bloc.dart';
+import 'package:dailylearningtracker/bloc/entry_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TaskScreen extends StatelessWidget {
+  final String nameOfTheTask;
+  const TaskScreen({super.key, required this.nameOfTheTask});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<EntryBloc, EntryState>(
+      builder: (context, state) {
+        if (state is StateShowEntries) {
+          final entry = state.entries.firstWhere(
+            (entry) => entry.nameOfTheTask == nameOfTheTask,
+          );
+          return Scaffold(
+            appBar: AppBar(),
+            body: ListView.builder(
+              itemCount: entry.learningDone.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(entry.learningDone[index].toString()),
+                );
+              },
+            ),
+          );
+        }
+        return Container();
+      },
+    );
+  }
+}
