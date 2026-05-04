bec21cb844d99c17479903b485464936f5a33bc4
sairaj15 <sairajssawant@proton.me>
2026-05-04 17:08:57 +0530

    feat: started working on local storage

--- lib/models/entry_model.dart
+++ lib/models/entry_model.dart
@@ -1,6 +1,10 @@
 class EntryModel {
   EntryModel({required this.nameOfTheTask, this.learningDone = const []});
 
+  Map<String, dynamic> toJson() {
+    return {'nameOfTheTask': nameOfTheTask, 'learningDone': learningDone};
+  }
+
   final String nameOfTheTask;
   final List<DateTime> learningDone;
 }
