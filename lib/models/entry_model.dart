18f536d66223b28da0297681be74b9223f0c326f
sairaj15 <sairajssawant@proton.me>
2026-05-26 23:19:17 +0530

    feat: updated EntryModel

--- lib/models/entry_model.dart
+++ lib/models/entry_model.dart
@@ -2,7 +2,12 @@ class EntryModel {
   EntryModel({required this.nameOfTheTask, this.learningDone = const []});
 
   Map<String, dynamic> toJson() {
-    return {'nameOfTheTask': nameOfTheTask, 'learningDone': learningDone};
+    return {
+      'nameOfTheTask': nameOfTheTask,
+      'learningDone': learningDone
+          .map((date) => date.toIso8601String())
+          .toList(),
+    };
   }
 
   final String nameOfTheTask;
