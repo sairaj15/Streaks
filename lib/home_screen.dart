677611941c1eca7a01d8e77b8bfd8ca31764fcba
sairaj15 <sairajssawant@proton.me>
2026-06-12 18:46:03 +0530

    refactor: refactored home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,10 @@
 import 'package:dailylearningtracker/bloc/entry_bloc.dart';
 import 'package:dailylearningtracker/bloc/entry_event.dart';
 import 'package:dailylearningtracker/bloc/entry_state.dart';
-import 'package:dailylearningtracker/statistics_screen.dart';
-import 'package:dailylearningtracker/task_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:dailylearningtracker/add_entry_screen.dart';
+import 'package:dailylearningtracker/statistics_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,7 +26,20 @@ class _HomeScreenState extends State<StatefulWidget> {
                 style: TextStyle(color: Colors.white),
               ),
               backgroundColor: Colors.blue,
-              //actions: [StatisticsScreen(state.entries)],
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.bar_chart),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            StatisticsScreen(entries: state.entries),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
             body: state.entries.isEmpty
                 ? Center(child: Text('No data entries available'))
